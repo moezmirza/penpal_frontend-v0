3cd9b5863574e8436d1c55d29e6e1ad8b5f19d7b
Abdul Moez <24100244@lums.edu.pk>
2024-07-09 01:50:51 +0500

    for preview changes on vercel 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { Outlet, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Login from "./routes/Login";
-import Register from "./routes/Register";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./services/firebase";
-import { useDispatch, useSelector } from "react-redux";
-import { setCurrentUser } from "./state/slices/userSlice";
-import { setAuth } from "./state/slices/authSlice";
-import { useLoginState } from "./hooks/useLoginState";
+import { Outlet, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './routes/Login';
+import Register from './routes/Register';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './services/firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentUser } from './state/slices/userSlice';
+import { setAuth } from './state/slices/authSlice';
+import { useLoginState } from './hooks/useLoginState';
 
 function App() {
   const isAuth = useLoginState();
@@ -16,12 +16,15 @@ function App() {
     return isAuth ? <Outlet /> : <Login />;
   };
   return (
-    <div className="">
+    <div className=''>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
         <Route element={<AuthenticatedRoutes />}>
-          <Route path="/" element={<h1>Hello Penpal here</h1>} />
+          <Route
+            path='/'
+            element={<h1>Hello Penpal here, Preview changes</h1>}
+          />
         </Route>
       </Routes>
     </div>
