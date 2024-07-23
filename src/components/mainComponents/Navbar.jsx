2040c1efd3c8abbd451e764a150dd0bdfacae4bd
Qa5imm <24100256@lums.edu.pk>
2024-07-23 23:41:22 +0500

    updated fix

--- src/components/mainComponents/Navbar.jsx
+++ src/components/mainComponents/Navbar.jsx
@@ -107,7 +107,7 @@ function NavbarOptions({ user, onSignout, onLinkClick, isAdmin }) {
           <li key={linkName} onClick={onLinkClick}>
             <Link
               to={adminNavbarLinkMap[linkName]}
-              className=" text-nowrap block px-4 py-1.5 md:py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              className="text-nowrap block px-4 py-1.5 md:py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
               {linkName}
             </Link>
@@ -129,7 +129,7 @@ function NavbarOptions({ user, onSignout, onLinkClick, isAdmin }) {
           <li key={linkName} onClick={onLinkClick}>
             <Link
               to={userNavbarLinkMap[linkName]}
-              className="block px-4 py-1.5 md:py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              className="block  px-4 py-1.5 md:py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
               {linkName}
             </Link>
